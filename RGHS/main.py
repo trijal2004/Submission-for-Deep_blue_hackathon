0a520f2e97d48fd391944ec849279a6523b47f93
rsh-lab <125012013+rsh-lab@users.noreply.github.com>
2024-01-03 04:52:51 +0530

    Update main.py
    convertd to function form

--- RGHS/main.py
+++ RGHS/main.py
@@ -15,29 +15,29 @@ if __name__ == '__main__':
     pass
 
 
-# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/RGHS"
-folder = "/home/trijal/Documents/GitHub/Single-Underwater-Image-Enhancement-and-Color-Restoration"
-path = folder + "/midframe"
-files = os.listdir(path)
-files =  natsort.natsorted(files)
+# # folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/RGHS"
+# folder = "/home/trijal/Documents/GitHub/Single-Underwater-Image-Enhancement-and-Color-Restoration"
+# path = folder + "/midframe"
+# files = os.listdir(path)
+# files =  natsort.natsorted(files)
 
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        img = cv2.imread(folder +'/midframe/' + file)
-        # img = cv2.imread('InputImages/' + file)
-        # path = np.unicode(path, 'utf-8')
-        # img = cv2.imread('InputImages/' + file)
-        # img = cv2.imread(np.unicode('InputImages/' + file, 'utf-8'))
+# for i in range(len(files)):
+#     file = files[i]
+#     filepath = path + "/" + file
+#     prefix = file.split('.')[0]
+#     if os.path.isfile(filepath):
+#         print('********    file   ********',file)
+#         img = cv2.imread(folder +'/midframe/' + file)
+#         # img = cv2.imread('InputImages/' + file)
+#         # path = np.unicode(path, 'utf-8')
+#         # img = cv2.imread('InputImages/' + file)
+#         # img = cv2.imread(np.unicode('InputImages/' + file, 'utf-8'))
 
-        # print('img',img)
-        height = len(img)
-        width = len(img[0])
+#         # print('img',img)
+#         height = len(img)
+#         width = len(img[0])
         # sceneRadiance = RGB_equalisation(img)
-
+def rghs(img)
         sceneRadiance = img
         # sceneRadiance = RelativeGHstretching(sceneRadiance, height, width)
 
@@ -47,4 +47,5 @@ for i in range(len(files)):
         sceneRadiance = LABStretching(sceneRadiance)
 
 
-        cv2.imwrite(folder +'/midframe_output/' + prefix + '_RGHS.jpg', sceneRadiance)
+       # cv2.imwrite(folder +'/midframe_output/' + prefix + '_RGHS.jpg', sceneRadiance)
+        return sceneRadiance
