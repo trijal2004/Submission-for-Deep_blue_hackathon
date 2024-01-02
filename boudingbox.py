73b717ef518bc14d4e1dd66be4c89ad18f3b1ad2
Arush <arush.up@gmail.com>
2024-01-03 03:42:37 +0530

    Added a noteboook for testing (remove from final submission

--- boudingbox.py
+++ boudingbox.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pytesseract
 from PIL import Image
 
-def show(image,image_name,gray = False,save = False):
+def show(image,image_name = "sus.jpg",gray = False,save = False):
     plt.figure(figsize=(12, 10))
     if gray :
         plt.imshow(image, cmap='gray')
@@ -15,6 +15,29 @@ def show(image,image_name,gray = False,save = False):
     if save :
         plt.savefig(image_name,bbox_inches = 'tight')
     plt.show()
+
+def final_bb(possible_contours,image):
+    # Find the minimum and maximum x and y coordinates of the boxes
+    min_x = min([d['x'] for d in possible_contours])
+    max_x = max([d['x'] + d['w'] for d in possible_contours])
+    min_y = min([d['y'] for d in possible_contours])
+    max_y = max([d['y'] + d['h'] for d in possible_contours])
+
+    copy = image.copy()
+    cv2.rectangle(copy, pt1=(min_x, min_y), pt2=(max_x, max_y), color=(255, 255, 255), thickness=2)
+    return copy
+
+def maximize_contrast(gray):
+
+    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    imgTopHat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, structuringElement)
+    imgBlackHat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, structuringElement)
+
+    imgGrayscalePlusTopHat = cv2.add(gray, imgTopHat)
+    gray = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+
+    return gray
 
 def drawbox(image_path):
     """
@@ -28,13 +51,7 @@ def drawbox(image_path):
     gray = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
 
     #maximizing contrast 
-    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-
-    imgTopHat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, structuringElement)
-    imgBlackHat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, structuringElement)
-
-    imgGrayscalePlusTopHat = cv2.add(gray, imgTopHat)
-    gray = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+    gray = maximize_contrast(gray)
 
     #Adaptive thresholding 
     img_blurred = cv2.GaussianBlur(gray, ksize=(5, 5), sigmaX=0)
@@ -56,7 +73,6 @@ def drawbox(image_path):
     )
 
     temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-
     cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
 
     # bouding box preparation 
@@ -80,9 +96,9 @@ def drawbox(image_path):
         })
 
     MIN_AREA = 10
-    MIN_WIDTH, MIN_HEIGHT = 0, 300
+    MIN_WIDTH, MIN_HEIGHT = 0, 400
     MAX_HEIGHT = 900
-    MIN_RATIO, MAX_RATIO = 0, 20
+    MIN_RATIO, MAX_RATIO = 0.20, 5
 
     possible_contours = []
 
@@ -103,17 +119,9 @@ def drawbox(image_path):
     #     cv2.drawContours(temp_result, d['contour'], -1, (255, 255, 255))
         cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x']+d['w'], d['y']+d['h']), color=(255, 255, 255), thickness=2)
 
+    show(temp_result)
     # Combined bounding box
-        
-    # Find the minimum and maximum x and y coordinates of the boxes
-    min_x = min([d['x'] for d in possible_contours])
-    max_x = max([d['x'] + d['w'] for d in possible_contours])
-    min_y = min([d['y'] for d in possible_contours])
-    max_y = max([d['y'] + d['h'] for d in possible_contours])
-
-    copy = img_ori.copy()
-    cv2.rectangle(copy, pt1=(min_x, min_y), pt2=(max_x, max_y), color=(255, 255, 255), thickness=2)
-
+    copy = final_bb(possible_contours,img_ori)
     show(image = copy, image_name = "image_with_bb.jpg",save = True)
-    
+
     return copy
